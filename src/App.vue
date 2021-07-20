6f41c6e125ac4e124ca9ba04e357667b428f14ff
imildositoe <imildositoe@gmail.com>
2021-07-20 23:35:50 +0200

    application clean

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld/>
+  <Index/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Index from './components'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Index
   }
 }
 </script>
